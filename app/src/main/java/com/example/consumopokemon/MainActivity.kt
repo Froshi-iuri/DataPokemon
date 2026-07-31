3cc4848aceaee6353e54df81144125f57bd8e11f
Froshi <kg5892868@gmail.com>
2026-07-30 21:29:31 -0500

    feat: API finish without listview

--- app/src/main/java/com/example/consumopokemon/MainActivity.kt
+++ app/src/main/java/com/example/consumopokemon/MainActivity.kt
@@ -1,18 +1,24 @@
 package com.example.consumopokemon
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.lifecycleScope
+import com.example.consumopokemon.databinding.ActivityMainBinding
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var listView: ListView
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         lifecycleScope.launch {
 
@@ -24,5 +30,16 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this@MainActivity, nombres, Toast.LENGTH_LONG).show()
         }
 
+        val nombres = response.results.map { it.name }
+
+        val adapter = ArrayAdapter(
+            this@MainActivity,
+            R.layout.simple_list_item_1,
+            nombres
+        )
+
+
+        binding.listPokemon.adapter = adapter
     }
+}
 }
